b3085d8e5e0c08510b65731cf0cef1fe87f8efc3
serashioda <seras37@gmail.com>
2016-07-27 02:33:24 -0700

    final comment modifications

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
-//
+// Master of JS
 'use strict';
 
-//
 var hours = ['6:00am', '7:00am', '8:00am', '9:00am', '10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm'];
 
-//
 var allLocations = [];
 
-//
+//OBJECT CONSTRUCTOR
 function Location(locationName, minCustPerHour, maxCustPerHour, avgCookiesPerCust) {
+//PROPERTIES
   this.locationName = locationName;
   this.minCustPerHour = minCustPerHour;
   this.maxCustPerHour = maxCustPerHour;
@@ -18,14 +17,16 @@ function Location(locationName, minCustPerHour, maxCustPerHour, avgCookiesPerCus
   this.totalDailyCookieSales = 0;
   allLocations.push(this);
 
-  this.calcCustEachHour = function() {
-//
+//METHODS; USE RANDOM GENERATOR TO CALC CUST EACH HOUR
+this.calcCustEachHour = function() {
+//FOR LOOP
     for (var i = 0; i < hours.length; i++) {
       var singleHourCust = Math.floor(Math.random() * (this.maxCustPerHour - this.minCustPerHour + 1)) + this.minCustPerHour;
       this.custEachHourArray.push(singleHourCust);
     }
   };
-//
+
+// USE PREV FUNCTION OUTPUT TO CALC # OF COOKIES SOLD EACH HOUR; USE .CEIL TO ROUND UP FOR WHOLE COOKIES
   this.calcCookiesEachHour = function() {
     this.calcCustEachHour();
     for (var i = 0; i < hours.length; i++) {
@@ -35,14 +36,14 @@ function Location(locationName, minCustPerHour, maxCustPerHour, avgCookiesPerCus
     }
   };
 
-// WILL TAKE ARRAY AND DISPLAY OUTCOME FROM FUNCTION INTO UNORDERED LIST
+// TAKE ARRAY AND DISPLAY OUTCOME FROM FUNCTION INTO UNORDERED LIST
   this.render = function() {
     this.calcCookiesEachHour();
   };
-//
+
   this.render();
 }
-//
+// LOC AND FORLOOP
 var firstAndPike = new Location('1st and Pike','23','65','6.3');
 var seaTac = new Location('SeaTac Airport','3','24','12');
 var seattleCenter = new Location('Seattle Center','11','38','3.7');
@@ -58,10 +59,10 @@ console.log(seaTac);
 console.log(seattleCenter);
 console.log(capitalHill);
 console.log(alki);
-//
+
 var locationTable = document.getElementById('locationjs');
 console.log('locationTable is', locationTable);
-//
+// MAKE HEADER ROWS
 function makeHeaderRow() {
 
   var tableRow = document.createElement('tr');
@@ -69,28 +70,31 @@ function makeHeaderRow() {
 //<th>firstAndPike</th>
   var thElement = document.createElement('th');
   thElement.textContent = '1st and Pike';
+//append to th element
   tableRow.appendChild(thElement);
 
 //<th>seaTac</th>
   var thElement = document.createElement('th');
   thElement.textContent = 'SeaTac Airport';
+//append to th element
   tableRow.appendChild(thElement);
 
 //<th>seattleCenter</th>
   var thElement = document.createElement('th');
   thElement.textContent = 'Seattle Center';
+//append to th element
   tableRow.appendChild(thElement);
 
 //<th>capitalHill</th>
   var thElement = document.createElement('th');
   thElement.textContent = 'Capital Hill';
+//append to th element
   tableRow.appendChild(thElement);
 
 //<th>alki</th>
   var thElement = document.createElement('th');
   thElement.textContent = 'Alki';
-  tableRow.appendChild(thElement);
-
+//append to th element(?)
   locationTable.appendChild(tableRow);
 };
 
@@ -98,34 +102,36 @@ function makeHeaderRow() {
 function makeAllTimesColumns() {
   var tableRow = document.createElement('tr');
 
-// MAKING EMPTY CELL FOR  1ST COLUMN
+//MAKING EMPTY CELL FOR  1ST COLUMN
   tableRow.appendChild(document.createElement('td'));
 
   for (var i=0; i < hours.length; i++){
     var tdElement = document.createElement('td');
     tdElement.textContent = hours[i];
+//APPEND TD ELEMENT
     tableRow.appendChild(tdElement);
     }
-//APPEND TO TABLEROW
+//APPEND TO TABLE ROW
   locationTable.appendChild(tableRow);
 }
 
 //FINAL FUNCTION TO MAKE ALL LOCATIONS ROWS
 function makeAllLocationRows() {
-
+//FOR LOOP
   for (var location = 0; location < allLocations.length; location++) {
     var tableRow = document.createElement('tr');
 
     var tdElement = document.createElement('td');
     tdElement.textContent = allLocations[location].locationName;
+//APPEND TO TD ELEMENT
     tableRow.appendChild(tdElement);
-
+//FOR LOOP
     for (var i = 0; i < allLocations[location].cookiesEachHourArray.length; i++) {
       var listElement = document.createElement('td');
       listElement.textContent = allLocations[location].cookiesEachHourArray[i];
       tableRow.appendChild(listElement);
     }
-
+//APPEND TO TABLE ROW
     locationTable.appendChild(tableRow);
   }
 }
